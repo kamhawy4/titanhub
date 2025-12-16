c4a75fcfcbba066f7a2125da1460190a3d0e1048
akamhawy <akamhawy@2p.com.sa>
2025-12-16 12:30:41 +0200

    fix bugs qabout migrations

--- app/Modules/Tasks/TaskServiceProvider.php
+++ app/Modules/Tasks/TaskServiceProvider.php
@@ -14,5 +14,9 @@ class TaskServiceProvider extends ServiceProvider {
 
     public function boot(){
         $this->loadRoutesFrom(__DIR__.'/UI/routes.php');
+
+        $this->loadMigrationsFrom(
+            __DIR__ . '/Infrastructure/Migrations'
+        );
     }
 }
